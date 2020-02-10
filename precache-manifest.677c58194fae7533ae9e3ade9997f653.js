5912779729696b55ffd239b884e191e5cdbaef39
clay-beach <30824350+clay-beach@users.noreply.github.com>
2020-02-10 20:18:27 +1000

    Updates

---
+++ precache-manifest.677c58194fae7533ae9e3ade9997f653.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290f524eda7f59be52c28eb95e6c36e9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7dd9f25c121ba78da2e0",
+    "url": "/Portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9709073a4f96cb0f20ae",
+    "url": "/Portfolio/static/css/main.cfc416d3.chunk.css"
+  },
+  {
+    "revision": "7dd9f25c121ba78da2e0",
+    "url": "/Portfolio/static/js/2.1f4078fe.chunk.js"
+  },
+  {
+    "revision": "60037e8887f425d039bd63775efa9319",
+    "url": "/Portfolio/static/js/2.1f4078fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9709073a4f96cb0f20ae",
+    "url": "/Portfolio/static/js/main.421a1149.chunk.js"
+  },
+  {
+    "revision": "38e9353468597077cd3e",
+    "url": "/Portfolio/static/js/runtime-main.ed271e06.js"
+  },
+  {
+    "revision": "ab83cce5b69a2dfeb90fef13c05bd828",
+    "url": "/Portfolio/static/media/1.ab83cce5.png"
+  },
+  {
+    "revision": "467d7acd4f75943ac0b3707618ca05bc",
+    "url": "/Portfolio/static/media/2.467d7acd.png"
+  },
+  {
+    "revision": "c1a3f13f832db24e85870d16e37019c9",
+    "url": "/Portfolio/static/media/3.c1a3f13f.png"
+  },
+  {
+    "revision": "90b402fd6bbf573768c40341ef1f5526",
+    "url": "/Portfolio/static/media/4.90b402fd.png"
+  },
+  {
+    "revision": "8516ee0f5643b850fd8e396c7bf0d630",
+    "url": "/Portfolio/static/media/code.8516ee0f.svg"
+  },
+  {
+    "revision": "f8ace35f27ab27b1880c8fd218ccc9e7",
+    "url": "/Portfolio/static/media/developer.f8ace35f.svg"
+  },
+  {
+    "revision": "b2986293c675e58932880dd8a316d8ab",
+    "url": "/Portfolio/static/media/mock.b2986293.jpg"
+  },
+  {
+    "revision": "97b5ee876f56be525a1f794e7c9e17f2",
+    "url": "/Portfolio/static/media/mock2.97b5ee87.jpg"
+  },
+  {
+    "revision": "bd1af0eee6343b583d5a1c98119bf8da",
+    "url": "/Portfolio/static/media/mock3.bd1af0ee.jpg"
+  },
+  {
+    "revision": "4611e75b31bfc4259b994a6c32dae4cd",
+    "url": "/Portfolio/static/media/mock4.4611e75b.jpg"
+  },
+  {
+    "revision": "9f44ae9c071e68c40baa084b23158fc6",
+    "url": "/Portfolio/static/media/movie-search.9f44ae9c.jpg"
+  }
+]);
